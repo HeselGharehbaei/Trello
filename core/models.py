a1d3d6cfea065b41a21149ff890b803b4ffef387
hesel <hgharehbaai@gmail.com>
2023-08-04 03:17:34 +0330

    add time stamp mixin  class to core/models.py

--- core/models.py
+++ core/models.py
@@ -5,9 +5,18 @@ from uuid import uuid4
 class BaseModel(models.Model):
     id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
 
+
     class Meta:
         abstract = True
 
+
+class TimeStampMixin(BaseModel):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        abstract = True
+                
 
 
 
