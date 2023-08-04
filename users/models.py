064528ce5b66ea0a13323f304c5d5db6d5de7aa7
hesel <hgharehbaai@gmail.com>
2023-08-04 21:14:30 +0330

    add class of user in users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from core.models import BaseModel
+from core.models import SoftDeleteModel
 
-# Create your models here.
+
+class User(AbstractUser, BaseModel, SoftDeleteModel):
+    email = models.EmailField(
+        _("email address"),
+        help_text=_("Email address of user"),  
+        unique=True,
+        blank=True
+    )
+    image = models.FileField(
+        _("Image"), 
+        upload_to="uploads/photos", blank= True, null= True
+    )
+
+
+    def __str__(self):
+        return f'{self.username}' 
+    
