610927435621c83277f2f75be7d7004b88f2e8bd
hesel <hgharehbaai@gmail.com>
2023-08-04 04:00:01 +0330

    add soft delete class in  core/models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from uuid import uuid4
+from django.db.models import QuerySet, Manager
+from django.utils import timezone
 
 
 class BaseModel(models.Model):
@@ -16,8 +18,31 @@ class TimeStampMixin(BaseModel):
 
     class Meta:
         abstract = True
-                
 
 
+class SoftQuerySet(QuerySet):
+    def delete(self):
+        return self.update(is_deleted = True, deleted_at = timezone.now())  
+    
+
+class SoftManager(Manager):
+    def get_queryset(self) -> QuerySet:
+        return SoftQuerySet(self.model, self._db).filter(Q(is_deleted=False) | Q(is_deleted__isnull= True))    
+    
+
+class SoftDeleteModel(models.Model):
+    objects = SoftManager()
+
+    is_deleted = models.BooleanField(null=True, blank=True, editable=False, db_index=True)
+    deleted_at = models.DateTimeField(null=True, blank=True, editable=False, db_index=True)
 
 
+    def delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+        
+
+    class Meta:
+        abstract = True    
+        
