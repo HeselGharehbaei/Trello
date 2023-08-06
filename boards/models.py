2a4aa33ae730797a33d8fa1ed064ce9421700131
PouriyaMn <pouriyamnejad@gmail.com>
2023-08-07 00:16:25 +0330

    create Board class in boards/models.py & some minor edit

--- boards/models.py
+++ boards/models.py
@@ -3,53 +3,78 @@ from core.models import BaseModel, TimeStampMixin
 from django.utils.translation import gettext_lazy as _
 
 
+class Board(BaseModel, TimeStampMixin):
+    workspace = models.ForeignKey(
+        "workspaces.Workspace",
+        on_delete=models.CASCADE,
+        related_name="boards",
+        verbose_name=_("Workspace"),
+    )
+    title = models.CharField(
+        _("Title"),
+        help_text=_("title of board"),
+        max_length=50,
+    )
+    description = models.TextField(
+        _("Description"),
+        help_text=_("description of board activities (optional)"),
+        blank=True,
+        null=True,
+    )
+    background_image = models.FileField(
+        upload_to="uploads/photos",
+        blank=True,
+        null=True,
+    )
+
+
 class Task(BaseModel, TimeStampMixin):
     list_ = models.ForeignKey(
         "List",
         on_delete=models.CASCADE,
         related_name="tasks",
-        verbose_name=_("List")
+        verbose_name=_("List"),
     )
     user = models.ForeignKey(
-        "User",
+        "users.User",
         on_delete=models.CASCADE,
         related_name="tasks",
-        verbose_name=_("User")
+        verbose_name=_("User"),
     )
     title = models.CharField(
         _("Title"),
-        help_text=_(" title of task"),
+        help_text=_("title of task"),
         max_length=50,
     )
     description = models.TextField(
         _("Description"),
         help_text=_("describe your task (optional)"),
-        null=True,
         blank=True,
+        null=True,
     )
     deadline = models.DateTimeField(
         _("Deadline"),
         help_text = _("specify a deadline (optional)"),
-        null=True,
         blank=True,
+        null=True,
     )
     start_date = models.DateTimeField(
         _("Start Date"),
         help_text=_("date of start doing task (optional)"),
-        null=True,
         blank=True,
+        null=True,
     )
     finished_date = models.DateTimeField(
         _("Finish Date"),
         help_text=_("date of when the task finished (optional)"),
-        null=True,
         blank=True,
+        null=True,
     )
     time_spent = models.TimeField(
         _("Time Spent"),
         help_text=_("the time you spent for doing this task (optional)"),
-        null=True,
         blank=True,
+        null=True,
     )
     order = models.DecimalField(
         max_digits=7,
