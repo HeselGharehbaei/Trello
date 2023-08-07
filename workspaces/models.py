b720c8d6866182552fafb3675670a96ffd4e33f3
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-08-07 23:11:27 +0330

    chenge sth

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,19 +1,17 @@
-from boards.models import Board
 from django.db import models
 from users.models import User
 from core.models import BaseModel , TimeStampMixin
 
 
-class Workspaces(BaseModel,TimeStampMixin):
+class Workspace(BaseModel,TimeStampMixin):
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='owned_workspaces')
+        User, on_delete=models.CASCADE, related_name='owned_workspace')
     title = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True, null=False)
     members = models.ManyToManyField(
-        User, through='WorkspacesMembership', through_fields=('Workspaces', 'member'))
+        User, through='WorkspacesMembership', through_fields=('Workspace', 'member'))
 
-    boards = models.ForeignKey(
-        Board,  related_name='boards')
+
 
     def __str__(self):
         return self.title
@@ -24,8 +22,8 @@ class WorkspacesMembership(TimeStampMixin, BaseModel):
         MEMBER = 1           
         ADMIN = 2             
 
-    Workspaces = models.ForeignKey(
-        Workspaces, on_delete=models.CASCADE)
+    Workspace = models.ForeignKey(
+        Workspace, on_delete=models.CASCADE)
     member = models.ForeignKey(
         User, on_delete=models.CASCADE)
     access_level = models.IntegerField(choices=Access.choices, default=1)
