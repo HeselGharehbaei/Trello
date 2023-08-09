9a9253cfe879101307c5135fd6ef4c320caf44e3
amir <amirmasoud.es80@gmail.com>
2023-08-09 16:27:44 +0330

    create comment class in boards /models.py

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,132 @@
 from django.db import models
+from core.models import BaseModel, TimeSTampMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Comment(BaseModel, TimeSTampMixin):
+    task = models.ForeignKey(
+        "Task",
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    text = models.TextField(
+        verbose_name=_("Comment"),
+        help_text=_("enter the comment"),
+        max_length=200,
+        null=True,
+        blank=True,
+    )
+
