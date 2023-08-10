dd7761e27d77f60847f3661ca8fc11583d594b24
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-08-10 16:44:29 +0330

    Workspace admin chenged

--- workspaces/admin.py
+++ workspaces/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Workspace, WorkspacesMembership
 
-# Register your models here.
+admin.site.register(Workspace)
+admin.site.register(WorkspacesMembership)
