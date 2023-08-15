03a75063e611cbeefc81cd0fba5f56dad80c8581
hesel <hgharehbaai@gmail.com>
2023-08-15 14:07:33 +0330

    add fieldsets in UserAdmin model

--- users/admin.py
+++ users/admin.py
@@ -9,3 +9,17 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'is_active', 'email', 'date_joined',)
     list_filter = ('is_active', 'is_staff')
     search_fields = ('username', 'first_name', 'last_name', 'email')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name','image', 'email',)}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff'),
+            'classes': ('collapse',)
+        }),
+        (_("Date Information"), {
+            'fields': ('date_joined',),
+            'classes': ('collapse',)
+        }),
+    )
+    
+
