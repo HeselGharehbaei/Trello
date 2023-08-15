87e2b86aa258d8c0da541bc3524ad0764655149c
hesel <hgharehbaai@gmail.com>
2023-08-15 14:05:51 +0330

    create UserAdmin model witl filter_display_lists_and_search_field

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
+from workspaces.models import Workspace
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_active', 'email', 'date_joined',)
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
