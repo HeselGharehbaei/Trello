fc78c7dad47d19101723118d15cbaab49ca1b749
PouriyaMn <pouriyamnejad@gmail.com>
2023-08-21 20:42:49 +0330

    feat: add [boards, users, workspaces].urls in trello/urls.py

--- trello/urls.py
+++ trello/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('boards/', include('boards.urls')),
+    path('users/', include('users.urls')),
+    path('workspaces/', include('workspaces.urls')),
 ]
