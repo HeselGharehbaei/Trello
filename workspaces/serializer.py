9bfc66f3d50a0645147f8513e55698f0be1421c6
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-08-22 16:51:46 +0330

    updeat WorkspaceSerializer and add meber and bord metods

--- workspaces/serializer.py
+++ workspaces/serializer.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 from .models import Workspace, WorkspacesMembership
+from users.models import User
+from users.serializers import UserSerializer
+
 
 class WorkspaceSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+    members = serializers.SerializerMethodField(many=True)
+    boards = serializers.SerializerMethodField()
+
+    def get_members(self, obj):
+        queryset = WorkspacesMembership.objects.filter(workspace=obj)
+        return WorkspacesMembershipSerializer(queryset, many=True, context={"request": self.context['request']}).data
+
+
+    def get_boards(self, obj):
+        return [board.name for board in obj.get_boards()]
+    
+
     class Meta:
         model = Workspace
-        fields = '__all__'  
+        fields = [
+            'id',
+            'owner',
+            'title',
+            'description',
+            'members'
+        ]
+        read_only_fields = ['owner']
+ 
 
 
 class WorkspacesMembershipSerializer(serializers.ModelSerializer):
@@ -12,27 +36,10 @@ class WorkspacesMembershipSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='member.username', read_only=True)
     email = serializers.CharField(source='member.email', read_only=True)
     
+
     
     class Meta:
         model = WorkspacesMembership
         fields = ['id',  'username',
                   'email',  'access_level']
 
-class WorkspaceWithMembersSerializer(serializers.ModelSerializer):
-    members = serializers.StringRelatedField(many=True)  
-    boards = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Workspace
-        fields = '__all__'
-
-    def get_boards(self, obj):
-        return [board.name for board in obj.get_boards()]
-    
-class WorkspacesMembershipDetailSerializer(serializers.ModelSerializer):
-    workspace = WorkspaceSerializer()
-    member = serializers.StringRelatedField()
-
-    class Meta:
-        model = WorkspacesMembership
-        fields = '__all__'
