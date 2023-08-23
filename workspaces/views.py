a3c1abbce4f4a43895ff6472bad7eeb6fea743c2
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-08-23 19:12:23 +0330

    add Workspacedetail view get metod

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import uuid
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from workspaces.models import Workspace, WorkspacesMembership
+from workspaces.serializer import WorkspacesMembershipSerializer,WorkspaceSerializer, WorkspaceshortSerializer
+from rest_framework import  status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from users.models import User
+
+
+class workspaceDetail(APIView):
+    serializer_class =  WorkspaceSerializer
+
+    def get(self, request, pk):
+        work = get_object_or_404(Workspace, pk=pk)
+        serializer = WorkspaceSerializer(work, context={"request": request})
+        return Response(serializer.data)
