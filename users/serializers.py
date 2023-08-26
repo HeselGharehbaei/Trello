0847636cd9839f65737faf9225a7817d3a586c66
hesel <hgharehbaai@gmail.com>
2023-08-26 10:22:01 +0330

    feat: write UserBriefSerializer and UserDetailSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -2,10 +2,20 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    user_owned_workspaces = serializers.StringRelatedField(many= True, source= "get_user_owned_workspace")
-    user_membered_workspaces = serializers.StringRelatedField(many= True, source= "get_user_membered_workspaces")
+class UserBriefSerializer(serializers.ModelSerializer):
+
 
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name", "email", "image", "user_owned_workspaces", "user_membered_workspaces")
+        fields = ("id", "username", "first_name", 
+                  "last_name", "email", 
+                  "image", 
+        )
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = User
+        fields =  '__all__'
