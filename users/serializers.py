c6b5d395fc6ce6a82fd29e7141bc6492a7275714
hesel <hgharehbaai@gmail.com>
2023-08-29 17:20:32 +0330

    feat: write serializer for user dashboard

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import User
 from workspaces.models import Workspace
+from boards.models import Task    
 
 
 class UserWorkspacesSerializer(serializers.ModelSerializer):
+
+
     class Meta:
         model = Workspace
         fields = ['id', 'title']
@@ -36,3 +39,32 @@ class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields =  '__all__'
+
+
+class CompletedTaskSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'finished_date', 'board_list']        
+
+
+class UserDashboardSerializer(serializers.ModelSerializer):
+    completed_task = CompletedTaskSerializer(
+        many= True, 
+        source= 'get_completed_task', 
+        read_only = True,
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'completed_task']  
+
+    def get_completed_task(self, instance):
+        completed_task_count = instance.get_completed_task().count()
+        return completed_task_count
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["completed_task_count"] = self.get_completed_task(instance)
+        return representation
