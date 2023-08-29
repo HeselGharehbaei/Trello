db9f5684a8c77dc31a9b5fba63141f17f0740a5c
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-08-29 15:57:46 +0330

    add database setting

--- trello/settings.py
+++ trello/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'trello.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "terllo",
+        'USER': 'postgres',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
