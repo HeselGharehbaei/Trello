4267dbc227601ad7fa1ef527644f54a0e41d40b7
hesel <hgharehbaai@gmail.com>
2023-08-29 14:12:50 +0330

    build: write IsOwnerOrReadOnlyInUserDetail custom permission

--- users/permissions.py
+++ users/permissions.py
@@ -8,12 +8,14 @@ class IsOwnerOrReadOnlyInUserDetail(BasePermission):
     message = 'permission denied, you are not the owner'
 
     def has_permission(self, request, view):
-        if request.method == 'GET':
-            return True
-        elif request.method == 'POST':
-            return request.user != request.user.is_authenticated
-        
+        if view.action == 'list':
+            return request.user.is_authenticated
+        if view.action == 'create':
+            return  request.user.is_authenticated == False
+        return request.user.is_authenticated
+    
     def has_object_permission(self, request, view, obj):
-        if request.method is SAFE_METHODS:
+        if view.action in ['list', 'create']:
             return True
         return obj == request.user
+    
